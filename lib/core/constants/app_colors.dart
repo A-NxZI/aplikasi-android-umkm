dd5694e10ca7ea6d2575102fc35e84361996fa8c
Aldy <aryaaldy2009@gmail.com>
2026-08-17 12:16:21 +0700

    adding global colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFEA4803);
+  static const textPrimary = Color(0xFF161823);
+  static const grey = Color(0xFF5C5C5C);
+  static const lightGrey = Color(0xFFF6F6F6);
+  static const placeholder = Color(0xFFD9D9D9);
+}
--- lib/features/home/views/home.dart
+++ lib/features/home/views/home.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-//warna yang dipake
-class AppColors {
-  static const primary = Color(0xFFEA4803);
-  static const textPrimary = Color(0xFF161823);
-  static const grey = Color(0xFF5C5C5C);
-  static const lightGrey = Color(0xFFF6F6F6);
-  static const placeholder = Color(0xFFD9D9D9);
-}
+import 'package:tes/core/constants/app_colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
