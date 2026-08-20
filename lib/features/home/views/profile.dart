004d3ddb3c4c683f3786be1bc8b47035a0c2d16a
Aldy <aryaaldy2009@gmail.com>
2026-08-20 15:54:54 +0700

    adding card profile

--- lib/features/home/views/profile.dart
+++ lib/features/home/views/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:tes/core/constants/app_colors.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,12 +12,75 @@ class ProfilePage extends StatelessWidget {
         title: const Text('Akun Saya',
             style: TextStyle(
               fontWeight: FontWeight.w600,
-              fontSize: 16,
+              fontSize: 20,
               color: AppColors.textPrimary,
-            )),
+              
+            )
+            ),
         backgroundColor: AppColors.background,
       ),
-      body: const Center(child: Text('Profile Page Content')),
+      body: Column(
+        children: [
+    Padding(padding: const EdgeInsets.symmetric(horizontal: 20),
+    
+    ),
+    const SizedBox(height: 20),
+    Container(
+      height: 98,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: AppColors.background,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 65,
+            height: 65,
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppColors.placeholder,
+            ),
+            child: const Icon(
+              Icons.person, 
+              size: 40,
+              color: AppColors.background,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Nama Pengguna',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                      color: AppColors.textPrimary,
+                    ),
+                  ),
+                  SizedBox(height: 1),
+                  Text(
+                    '0800-123-4567',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                      color: AppColors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    )
+        ],
+      ),
     );
   }
 }
